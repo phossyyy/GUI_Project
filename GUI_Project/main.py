cc4094ce09ddbc4d7becc49e9e0dc853334a0761
Alexander Tellez <alexthegreat09072007@gmail.com>
2023-11-17 09:56:16 -0800

    Changing file order

--- GUI_Project/main.py
+++ GUI_Project/main.py
@@ -7,14 +7,14 @@ from game import user
 pygame.init()
 
 #Loading Images
-GRASS = pygame.image.load("assets/imgs/grass.jpg")
-TRACK = pygame.image.load("assets/imgs/track.png")
+GRASS = pygame.image.load("assets/grass.jpg")
+TRACK = pygame.image.load("assets/track.png")
 
-TRACK_BORDER = pygame.image.load("assests/imgs/track-border.png")
-FINISH = pygame.image.load("assets/imgs/finish.png")
+TRACK_BORDER = pygame.image.load("assests/track-border.png")
+FINISH = pygame.image.load("assets/finish.png")
 
-RED_CAR = pygame.image.load("assets/imgs/red-car.png")
-GREEN_CAR = pygame.image.load("assets/imgs/green-car.png")
+RED_CAR = pygame.image.load("assets/red-car.png")
+GREEN_CAR = pygame.image.load("assets/green-car.png")
 
 WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_hieght()
 

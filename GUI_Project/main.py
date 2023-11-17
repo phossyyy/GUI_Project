b3b06dfa54914312edfe4897ad9beacb2b3e1300
Alexander Tellez <alexthegreat09072007@gmail.com>
2023-11-17 09:50:24 -0800

    ADDING THE ASSETS FOR THE GAME

--- GUI_Project/main.py
+++ GUI_Project/main.py
@@ -1,8 +1,23 @@
 import pygame
+import math
+import time
 from game import user
 
 #Initialize Pygame
 pygame.init()
+
+#Loading Images
+GRASS = pygame.image.load("assets/imgs/grass.jpg")
+TRACK = pygame.image.load("assets/imgs/track.png")
+
+TRACK_BORDER = pygame.image.load("assests/imgs/track-border.png")
+FINISH = pygame.image.load("assets/imgs/finish.png")
+
+RED_CAR = pygame.image.load("assets/imgs/red-car.png")
+GREEN_CAR = pygame.image.load("assets/imgs/green-car.png")
+
+WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_hieght()
+
 
 #Make Framerate
 clock = pygame.time.Clock()
@@ -15,10 +30,7 @@ WHITE =  (255, 255, 255)
 BLACK = (0, 0, 0)
 
 #making a game window
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 600
-
-screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+screen = pygame.display.set_mode(( WIDTH , HEIGHT ))
 pygame.display.set_caption("GUI_Project")
 
 #GAME LOOP
@@ -28,6 +40,11 @@ while run:
     
     #FRAMERATE
     clock.tick(FPS)
+
+    screen.blit(GRASS (0, 0))
+
+    #UPDATE THE SCREEN
+    pygame.display.update()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
